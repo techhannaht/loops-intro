76abf6137ceebf3c76ffe7e3805b81b18b4cd53b
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-08-29 11:15:08 -0400

    Loops exercises completed because I am a rockstar.

---
+++ main.js
@@ -0,0 +1,15 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+}
+
+
+var myInterests = ["walking", "reading", "playing video games", "cooking", "drawing"];
+
+for (let i = 0; i < myInterests.length; i++)
+if (myInterests[i] == "reading") {
+        console.log(`My absolute favorite interest is: ${myInterests[i]}`);
+    } else {
+        console.log(`One of my interests is: ${myInterests[i]}`)
+}
+   
